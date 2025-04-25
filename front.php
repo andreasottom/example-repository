2968f0cacf59a5d430dd70bd03add76c10053fe4
andreasottom <andreajoy.sotto@email.lcup.edu.ph>
2025-04-25 09:00:20 +0800

    Add files via upload

---
+++ front.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+	<h2>LOGIN FORM</h2>
+	<form action="login.php" method="POST">
+
+		<label>Username: </label>
+		<input type="text" name="username" required>
+		<br><br>
+
+		<label>Password: </label>
+		<input type="password" name="password" required>
+		<br><br>
+
+		<button type="submit">LOGIN</button>
+
+	</form>
+
+</body>
+</html>
